97a736695243bb2be0d570004a14c30ca826869f
MARIN-Zenin <marin.lucas009@gmail.com>
2025-05-14 14:48:57 -0300

    Adequando css nav/header

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,50 @@
+import "./Header.css";
+import "./Navbar.css";
+
+
+function Header() {
+  return (
+    <>
+      <header>       
+          <h1>oin</h1>
+      </header>
+
+    <nav>
+          <a href="/">
+            inicio
+          </a>
+
+          <a href="/">
+            page 1
+          </a>
+
+          <a href="/">
+            page 2
+          </a>
+
+          <a href="/">
+            page 3
+          </a>
+
+          <a href="/">
+            page 4
+          </a>
+
+          <a href="/">
+            page 5
+          </a>
+
+          <a href="/">
+            page 6
+          </a>
+
+          <a href="/">
+            page 7
+          </a>
+
+    </nav>
+   </>
+  );
+}
+
+export default Header;
