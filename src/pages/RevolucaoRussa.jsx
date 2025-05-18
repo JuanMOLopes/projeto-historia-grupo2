b13e458a4714d5ff185638d2188792535d92047f
Juan Lopes <juanzinho.matheus@gmail.com>
2025-05-17 23:28:59 -0300

    Video

--- src/pages/RevolucaoRussa.jsx
+++ src/pages/RevolucaoRussa.jsx
@@ -18,9 +18,12 @@ function RevolucaoRussa() {
           <iframe
             width="100%"
             height="400"
-            src=""
-            title=""
-            allowFullScreen
+            src="https://www.youtube.com/embed/XpxprM5lcm0?si=wcTKUVVFSHLeH07k"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
           ></iframe>
         </div>
 
