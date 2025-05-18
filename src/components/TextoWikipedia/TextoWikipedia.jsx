fb9a0de1a8a4c6f982e393ab0146787ae0aadb00
Juan Lopes <juanzinho.matheus@gmail.com>
2025-05-17 23:24:05 -0300

    API Wikipedia

---
+++ src/components/TextoWikipedia/TextoWikipedia.jsx
@@ -0,0 +1,59 @@
+import "./TextoWikipedia.css";
+import { useState, useEffect } from "react";
+
+function TextoWikipedia(props) {
+  const [conteudo, setConteudo] = useState("");
+  const [erro, setErro] = useState("");
+
+  useEffect(() => {
+    // criamos uma função assíncrona (espera a função terminar enquanto o resto do código continua executando)
+    async function fetchResumo() {
+      // tentamos pegar o conteúdo da Wikipedia
+      try {
+        // tradução: await = esperar, fetch = buscar
+        // await fetch significa que o código vai esperar a resposta da busca na URL antes de continuar
+        // o fetch busca o conteúdo da URL
+        // passamos o tema como props
+        const resposta = await fetch(
+          `https://pt.wikipedia.org/api/rest_v1/page/summary/${props.tema}`
+        );
+        // se a resposta for diferente de ok (ruim), lança um erro
+        // tradução: throw new Error = lançar novo erro
+        // a resposta diferente de ok geralmente é quando o servidor respondeu, mas o conteúdo não foi encontrado
+        if (!resposta.ok) {
+          throw new Error("Conteúdo não encontrado");
+        }
+
+        // se a resposta for ok, convertemos a resposta em JSON
+        const dados = await resposta.json();
+        console.log(dados);
+
+        // pegamos o conteúdo do resumo e colocamos no estado Conteudo
+        setConteudo(dados.extract);
+      } catch (erro) {
+        // se o try der erro, pegamos o erro e colocamos no estado Erro
+        // geralmente é quando esta sem internet
+        setErro(erro.message);
+      }
+    }
+
+    // chamamos a função fetchResumo
+    fetchResumo();
+  }, []);
+  // o useEffect é executado apenas uma vez, quando a página é carregada
+
+  // se der erro, mostramos a mensagem de erro
+  if (erro) return <h2>Erro: {erro}</h2>;
+
+  return (
+    <div className="TextoWikipedia">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Wikipedia-logo-v2-en.svg/640px-Wikipedia-logo-v2-en.svg.png"
+        alt=""
+      />
+      <p>{conteudo}</p>
+    </div>
+  );
+}
+
+export default TextoWikipedia;
--- src/pages/RevolucaoRussa.jsx
+++ src/pages/RevolucaoRussa.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
 import TextoDireita from "../components/TextoDireita/TextoDireita";
+import TextoWikipedia from "../components/TextoWikipedia/TextoWikipedia";
 
 function RevolucaoRussa() {
   return (
@@ -10,7 +11,7 @@ function RevolucaoRussa() {
 
       <div className="conteudo">
         <div className="secao-resumo-wikipedia">
-          
+          <TextoWikipedia tema="Revolução Russa de 1917" />
         </div>
 
         <div className="secao-video">
