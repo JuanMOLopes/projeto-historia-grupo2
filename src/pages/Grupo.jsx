5afffa7a805b75f9e2a52863e8915ce720bff863
MARIN-Zenin <marin.lucas009@gmail.com>
2025-05-21 10:31:27 -0300

    Correção de conflitos

--- src/pages/Grupo.jsx
+++ src/pages/Grupo.jsx
@@ -1,3 +1,4 @@
+import "../App.css"
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import GrupoImg from "../../public/Grupo2Foto.jpeg";
@@ -6,16 +7,16 @@ function Grupo() {
   return (
     <>
       <Header titulo="Grupo 2" />
-
-      <div className="fotoGrupo">
         <img className="Grupo" src={GrupoImg} alt="Grupo 2" />
-      </div>
+
+        <div className="Container-grp2">
       <p>
         Nós do grupo 2 escrevemos esta mensagem como forma de agradecimento aos professores Julia, Antônio e Leandro por esta atividade que foi proposta. Durante o processo de criação do site, diversos pontos do grupo foram melhorados, tendo como mais importante o trabalho em equipe. Essa atividade foi de grande importância para nossa aprendizagem e estudo tanto de conteúdos de desenvolvimento de sistemas, como também de história.
       </p>
       <p>
         O grupo 2 reitera sua gratidão pelo trabalho proposto pelos profissionais do ensino, em prol de obtermos conhecimento. Gratos!
       </p>
+      </div>
 
       <Footer />
     </>
